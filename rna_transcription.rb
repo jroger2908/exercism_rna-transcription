8a7ee928af81f845e702bd584b4416f5bd52141e
JR <jroger2908@gmail.com>
2016-03-09 20:46:48 -0500

    final solution

--- rna_transcription.rb
+++ rna_transcription.rb
@@ -1,5 +1,18 @@
 class Complement
+   VERSION = 3
+   DNA_TO_RNA = {"G" => "C", "C" => "G","T" => "A", "A" => "U"}
+   
   def self.of_dna(nucleotide)
-
+    translate(DNA_TO_RNA, nucleotide)
+  end
+ 
+  def self.translate(dna_strand, nucleotide)
+    final = ""
+    nucleotide.each_char do |n|
+      raise ArgumentError unless dna_strand.keys.include? n
+      final << dna_strand[n]
+    end
+    final
   end
 end
+
